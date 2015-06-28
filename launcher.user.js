a6042b53ba3a7b9b463a6ce8346005e0276089d2
prorokma <mobilepro@email.cz>
2015-06-28 11:58:59 +0200

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -973,7 +973,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  names = ["NotReallyABot"],
+  names = ["Earth"],
   originalName = names[Math.floor(Math.random() * names.length)],
   sessionScore = 0,
   serverIP = "",
